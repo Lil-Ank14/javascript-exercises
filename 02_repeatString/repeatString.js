c7e5fe77479173a5edd44e3c25d757db74722182
Ankur Senapati <asenapati2020@gmail.com>
2024-06-21 18:09:13 -0400

    Solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, repeat) {
+  let initialWord = word;
+  if (repeat == 0) {
+    return "";
+  }
+  if (repeat < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < repeat - 1; i++) {
+    word = word + initialWord;
+  }
+  return word;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
