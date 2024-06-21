18f1790adda4d30f7a7acfa6b267bdf813e8af05
Ankur Senapati <asenapati2020@gmail.com>
2024-06-21 18:23:42 -0400

    Finished 3rd challenge

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  wordArray = word.split("");
+  let newWord = "";
+  for (let i = 0; i < wordArray.length; i++) {
+    newWord = wordArray[i] + newWord;
+    console.log(newWord);
+  }
+  return newWord;
 };
 
 // Do not edit below this line
