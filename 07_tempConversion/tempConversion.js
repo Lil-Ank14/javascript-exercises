5b6cfee29366bd5a18f7c90ad336d45689214774
Ankur Senapati <asenapati2020@gmail.com>
2024-06-21 22:43:45 -0400

    Solve challenge 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degreeF) {
+  let celsius = (degreeF - 32) * (5 / 9);
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreeC) {
+  let farenheight = degreeC * (9 / 5) + 32;
+  farenheight = Math.round(farenheight * 10) / 10;
+  return farenheight;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
